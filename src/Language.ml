c1f5665832326b4b3eba79bf43fbca5776aea636
alenasaz <aLeNka_saZaNova@mail.ru>
2019-04-01 10:49:31 +0300

    New changes

--- src/Language.ml
+++ src/Language.ml
@@ -151,26 +151,26 @@ module Stmt =
        which returns a list of formal parameters and a body for given definition
     *)
      let toBool b = b <> 0
-    let rec eval env (st, input, output) op =
+    let rec eval env (st, i, o) op =
          match op with
-        | Read    v       -> (State.update v (List.hd input) st, List.tl input, output)
-        | Write   e       -> (st, input, output @ [Expr.eval st e])
-        | Assign (v, e)   -> (State.update v (Expr.eval st e) st, input, output)
-        | If (e1, e2, e3) -> if (Expr.eval st e1) != 0 then eval env (st, input, output) e2 else eval env (st, input, output) e3
-        | While (e1, e2)  -> if toBool (Expr.eval st e1) then eval env (eval env (st, input, output) e2) op else (st, input, output)
-        | Seq    (e1, e2) -> eval env (eval env (st, input, output) e1) e2
-        | Skip            -> (st, input, output)
+        | Read    v       -> (State.update v (List.hd i) st, List.tl i, o)
+        | Write   e       -> (st, i, o @ [Expr.eval st e])
+        | Assign (v, e)   -> (State.update v (Expr.eval st e) st, i, o)
+        | If (e1, e2, e3) -> if (Expr.eval st e1) != 0 then eval env (st, i, o) e2 else eval env (st, i, o) e3
+        | While (e1, e2)  -> if toBool (Expr.eval st e1) then eval env (eval env (st, i, o) e2) op else (st, i, o)
+        | Seq    (e1, e2) -> eval env (eval env (st, i, o) e1) e2
+        | Skip            -> (st, i, o)
         | Repeat (e1, e2)  ->
-            let (st, input, output) = eval env (st, input, output) e1 in
+            let (st, input, output) = eval env (st, i, o) e1 in
             let r = Expr.eval st e2 in
-            if r != 0 then (st, input, output) else eval env (st, input, output) op
+            if r != 0 then (st, i, o) else eval env (st, i, o) op
         | Call (name, args) ->
             let (arg_names, locals, body) = env#definition name in
             let args = List.combine arg_names (List.map (Expr.eval st) args) in
             let state = State.push_scope st (arg_names @ locals) in
             let fun_env_w_args = List.fold_left (fun st (name, value) -> State.update name value st) state args in
-            let (new_s, input, output) = eval env (fun_env_w_args,input, output) body in
-            (State.drop_scope new_s st, input, output)
+            let (new_s, i, o) = eval env (fun_env_w_args,i, o) body in
+            (State.drop_scope new_s st, i, o)
 
 
     (* Statement parser *)
@@ -184,7 +184,7 @@ module Stmt =
         | "if" e1:!(Expr.parse) "then" e2:parse e3:elif {If (e1, e2, e3)}
         | "skip" {Skip}
         | "while" e1:!(Expr.parse) "do" e2:parse "od" {While (e1, e2)}
-        | "repeat" e1:parse "until" e2:!(Expr.parse) {RepeatUntil  (e1, e2)}
+        | "repeat" e1:parse "until" e2:!(Expr.parse) {Repeat  (e1, e2)}
         | "for" e1:parse "," e2:!(Expr.parse) "," e3:parse "do" s:parse "od" {Seq (e1, While (e2, Seq(s, e3)))}
         | name:IDENT "(" args:(!(Expr.parse))* ")" {Call (name, args)};
       
